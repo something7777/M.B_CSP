ef34252d8c713393af84691d2406d9fd849ac80d
something7777 <mayzi.bless@ucas-edu.net>
2026-01-23 14:42:34 -0700

    numbers notes

---
+++ notes/integers_floats.py
@@ -0,0 +1,14 @@
+"""price = float(input("how much did it cost: $"))
+
+sales_tax = float(input("how much is the sales tax: %"))/100
+
+
+total = round(price + (price*sales_tax), 2)
+
+print(total)"""
+
+apples = int(input("how many apples are there: "))
+
+friends = 8
+
+print("each friend can have", apples//friends, "and i have", apples%friends, "left over")
---
+++ practice/variables_practice.py
@@ -0,0 +1,23 @@
+# MB variables Practice 
+
+name = "bubbles"
+
+number = 7
+
+big_number = 786
+
+breakfast = "waffles"
+
+favorite_color = "purple"
+
+school = "UCAS"
+
+year = 2026
+
+eye_color = "dark brown"
+
+age = 14
+
+favorite_school_subject = "study hall"
+
+print(name, number, big_number, breakfast, favorite_color, school, year, eye_color, age, favorite_school_subject)
