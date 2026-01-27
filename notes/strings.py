9c23e8beb1d1b74d56d4d948c823f5e27ee631a3
something7777 <mayzi.bless@ucas-edu.net>
2026-01-27 14:42:12 -0700

    strings notes

---
+++ notes/strings.py
@@ -0,0 +1,17 @@
+#mb strings.py
+#name = input("what is your name?: ").strip().capitalize().title()
+
+#print("hello", name + "!")
+
+"""sentence = "the quick brown fox jumps over the lazy dog."
+word = input("tell me a word from the sentence: ")
+new_word = input("give me a new word: ")
+start = sentence.find(word)
+length = len(word)
+print(sentence.replace(word, new_word))
+
+#print(sentece[start:start=length])"""
+
+num_one = int(input("tell me a number; "))
+num_two = int(input("tell me a number; "))
+print(num_one*num_two)
