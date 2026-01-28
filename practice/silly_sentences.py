88f9a0ea74d3d1704a8570d0f1540787494e0f1e
something7777 <mayzi.bless@ucas-edu.net>
2026-01-28 14:34:07 -0700

    silly sentences

---
+++ practice/silly_sentences.py
@@ -0,0 +1,9 @@
+#MB silly sentences 
+
+noun1 = input("give me a noun: ").strip().lower()
+
+adjective = input("give me an adjective: ").strip().lower()
+
+noun2 = input("give me another noun: ").strip().lower()
+
+print("After hiding the painting in his "+ noun1 +" for two years, he grew "+ adjective +" and tried to sell it to a/an "+ noun2 +" in Florance, but he was caught:(")
