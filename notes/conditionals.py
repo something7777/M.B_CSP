b31dafb572792b34be2a187a0a6bd93ffa606083
something7777 <mayzi.bless@ucas-edu.net>
2026-01-30 14:40:13 -0700

    issac boo

---
+++ notes/conditionals.py
@@ -0,0 +1,21 @@
+"""number = -18 
+
+if number < 10 and number > -10:
+    print(f"{number} is a single digit number")
+elif abs(number) < 100:
+    print(f"{number} is a double digit number")
+else:
+    print(f"{number} is a double digit number")
+
+print("End of the program")"""
+
+name = input("What is your name: ").strip().capitalize()
+
+if name == "Arthur":
+    print("welcome King Arthur.")
+    quest = input("what is your quest: ").strip().title()
+    if quest == "Holy Grail"
+else:
+    print(f"{quest} lame quest but wtv i get it LOSER")
+else
+    print(f"hello {name}!")
---
+++ notes/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+number = -18 
+
+if number < 10 and number is > -10:
+    print(f"{number} is a single digit number")
+
+print("End of the program")
