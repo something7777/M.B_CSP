dba278cc2fa7dade5663939bca86f94e96d48e50
something7777 <mayzi.bless@ucas-edu.net>
2026-02-23 14:41:43 -0700

    financial calculator

---
+++ C/practices/financial_calculator.c
@@ -0,0 +1,28 @@
+//MB, Period7, financial_calculator.c
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+    float number;
+    (char)printf("what is your monthly income?\n");
+    scanf("%d", (float)number);
+    float number1;
+    (char)printf("what is your monthly rent/mortgage?\n");
+    scanf("%d", (float)number1);
+
+    float number2;
+    (char)printf("what is your monthly utilities cost?\n");
+    scanf("%d", (float)number2);
+
+    float number3;
+    (char)printf("what is your monthly groceries cost?\n");
+    scanf("%d", (float)number3);
+    float number4;
+    (char)printf("what is your monthly transportation cost?\n");
+    scanf("%d", (float)number4);
+
+
+
+ return 0; 
+}
